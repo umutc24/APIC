d6d13b1e1b041db0865f800b46ffd64999424608
Umut Ceylan <umutceylan@Umuts-MacBook-Pro.local>
2021-11-15 17:36:46 +0000

    update

---
+++ APIC.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+import sys
+sys.path.append('/Library/Frameworks/Python.framework/Versions/3.9/lib/python3.9/site-packages')
+import acitoolkit.acitoolkit as aci
+
+APIC_URL = 'https://sandboxapicdc.cisco.com'
+USERNAME = 'admin'
+PASSWORD = '!v3G@!4@Y'
+
+# Login to APIC
+SESSION = aci.Session(APIC_URL, USERNAME, PASSWORD)
+RESP = SESSION.login()
+if not RESP.ok:
+    print('Could not login to APIC')
+    sys.exit()
+
+ENDPOINTS = aci.Endpoint.get(SESSION)
+print('{0:19s}{1:14s}{2:10s}{3:8s}{4:17s}{5:10s}'.format("MAC ADDRESS","IP ADDRESS","ENCAP","TENANT","APP PROFILE","EPG"))
+print('-'*80)
+
+for EP in ENDPOINTS:
+    epg = EP.get_parent()
+    app_profile = epg.get_parent()
+    tenant = app_profile.get_parent()
+    print('{0:19s}{1:14s}{2:10s}{3:8s}{4:17s}{5:10s}'.format(EP.mac, EP.ip, EP.encap, tenant.name, app_profile.name, epg.name))
